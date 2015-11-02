233645b5061af40486d1251b19c549c11e26224f
Your Name <beatrizcl.14@gmail.com>
2015-11-02 14:47:11 +0100

    Ejercicio 7- client.py

--- client.py
+++ client.py
@@ -10,17 +10,23 @@ import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = sys.argv[1]
-PORT = sys.argv[2]
-METODO = sys.argv[3]
-LOGIN = sys.argv[4]
+try:
+    SERVER = sys.argv[1]
+    PORT = sys.argv[2]
+    METODO = sys.argv[3]
+    LOGIN = sys.argv[4]
+    EXPIRES = sys.argv[5]
+except IndexError:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, int(PORT)))
-print("Enviando: " + "REGISTER sip:" + LOGIN + " SIP/2.0" + "\r\n\r\n")
-my_socket.send("REGISTER sip:" + LOGIN + " SIP/2.0")
+print("Enviando: " + "REGISTER sip:" + LOGIN + " SIP/2.0" + "\r\n")
+print("Expires: " + EXPIRES + "\r\n\r\n")
+my_socket.send("REGISTER sip:" + LOGIN +
+               " SIP/2.0" + "Expires: " + str(EXPIRES))
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
