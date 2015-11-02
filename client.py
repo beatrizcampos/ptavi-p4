aaf0f412f5c6f8909af745ae1a8e776a0c9d679c
Your Name <beatrizcl.14@gmail.com>
2015-11-02 17:50:31 +0100

    Ejercicio Finalizado y pasado pep8 - client.py

--- client.py
+++ client.py
@@ -9,25 +9,27 @@ import sys
 
 # Cliente UDP simple.
 try:
-    #(SERVER, PORT, METODO, LOGIN, EXPIRES) = sys.argv
-    SERVER = sys.argv[1]
-    PORT = sys.argv[2]
-    METODO = sys.argv[3]
-    LOGIN = sys.argv[4]
-    EXPIRES = sys.argv[5]
 
-except IndexError, ValueError:
+    (_, SERVER, PORT, METODO, LOGIN, EXPIRES) = sys.argv
+
+except IndexError:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, int(PORT)))
-print("Enviando: " + "REGISTER sip:" + LOGIN + " SIP/2.0" + "\r\n")
-print("Expires: " + EXPIRES + "\r\n\r\n")
-my_socket.send("REGISTER sip:" + LOGIN +
-               " SIP/2.0" + "Expires: " + str(EXPIRES))
+
+Mensaje_Metodo = (METODO.upper() + " sip:" + LOGIN + " SIP/2.0\r\n")
+Mensaje_Expire = ("Expires: " + str(EXPIRES) + "\r\n")
+print("Enviando: " + Mensaje_Metodo + Mensaje_Expire)
+
+# Lo enviamos
+my_socket.send(bytes(Mensaje_Metodo, 'utf-8')
+               + bytes(Mensaje_Expire, 'utf-8') + b'\r\n')
+
 data = my_socket.recv(1024)
+
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
